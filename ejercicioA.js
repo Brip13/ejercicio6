d96a4176602912ba6c4754c3cd497a2443a69085
Brip13 <brigp138@gmail.com>
2024-09-16 23:43:06 -0600

    Agregue archivo js

---
+++ ejercicioA.js
@@ -0,0 +1,13 @@
+console.log ('EJERCICIO A:');
+
+var cantidadDeGatos=8;
+var gatitos = ['😺', '😸', '😹'];
+
+function mostrarGatitos(cantidad){
+    for (var i=0; i<cantidad; i++){
+        var emoji=gatitos [i%gatitos.length];
+        console.log('Gato #' + (i+1) + emoji);
+    }
+}
+
+mostrarGatitos(cantidadDeGatos);
